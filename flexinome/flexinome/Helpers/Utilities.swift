4bf7c2a63f1cbb8ee668ad5d481612aea196aa2c
yanghykevin <yanghykevin@outlook.com>
2021-02-27 10:27:42 -0500

    added Utilities file

---
+++ flexinome/flexinome/Helpers/Utilities.swift
@@ -0,0 +1,22 @@
+//
+//  Utilities.swift
+//  flexinome
+//
+//  Created by Kevin Yang on 2021-02-26.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
