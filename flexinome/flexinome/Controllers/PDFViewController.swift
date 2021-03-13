f678ac4f73cc2987f74b8163c60195fb8a948f25
yanghykevin <yanghykevin@outlook.com>
2021-03-13 15:20:29 -0500

    Minor UI fix in Metronome page; Begin to embed metronome in pdf viewer

--- flexinome/flexinome/Controllers/PDFViewController.swift
+++ flexinome/flexinome/Controllers/PDFViewController.swift
@@ -11,26 +11,44 @@ import PDFKit
 class PDFViewController: UIViewController {
     
     var pdfView = PDFView()
-        var pdfURL: URL!
-
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            view.addSubview(pdfView)
-            
-            if let document = PDFDocument(url: pdfURL) {
-                pdfView.displayMode = .singlePage
-                pdfView.autoScales = true
-                pdfView.displayDirection = .horizontal
-                pdfView.usePageViewController(true)
-                pdfView.document = document
-            }
-            self.hideNavigationBarWhenTappedAround()
-        }
+    var pdfURL: URL!
+    
+    private let metronomeButton: UIButton = {
+        let button = UIButton()
+        button.clipsToBounds = true
+        button.setBackgroundImage(UIImage(named: "metronome_color"), for: .normal)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        override func viewDidLayoutSubviews() {
-            pdfView.frame = view.frame
+        view.addSubview(pdfView)
+        view.addSubview(metronomeButton)
+        metronomeButton.addTarget(self, action: #selector(metronomeButtonTapped), for: .touchUpInside)
+        
+        if let document = PDFDocument(url: pdfURL) {
+            pdfView.displayMode = .singlePage
+            pdfView.autoScales = true
+            pdfView.displayDirection = .horizontal
+            pdfView.usePageViewController(true)
+            pdfView.document = document
         }
+        self.hideNavigationBarWhenTappedAround()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        pdfView.frame = view.frame
+        
+        metronomeButton.frame = CGRect(x: view.frame.maxX-70 , y: view.frame.maxY*0.9, width: 60, height: 60)
+    }
+    
+    // open metronome page in a modal view
+    @IBAction func metronomeButtonTapped() {
+        
+        let vc = self.storyboard?.instantiateViewController(identifier: "MetronomeViewController")
+        self.present(vc!, animated: true, completion: nil)
+    }
                     
 
     /*
